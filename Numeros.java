169e6c3fe1d8faebf65e268d748f25d3e97d2997
Mia <a_javier.ariza.hidalgo@iespablopicasso.es>
2021-11-25 13:03:23 +0100

    Añado el archivo al repositorio

---
+++ Numeros.java
@@ -0,0 +1,24 @@
+package Tema5;
+import java.util.Scanner;
+public class ej16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduzca un numero");
+        int num = Integer.parseInt(sc.nextLine());
+
+        boolean primo = true;
+
+        for (int i = 2; i < num; i++) {
+            if ((num%0)==0) {
+                primo = false;
+            }
+            
+        }
+        if (primo) {
+            System.out.println("El numero es primo");
+        } else {
+            System.out.println("El numero no es primo");
+        }
+    }
+}
